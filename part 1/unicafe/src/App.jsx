08ea533b6bda8e9283f6859cfb7b4e97b9a2b03a
fatboss19820822 <raymond19820822@gmail.com>
2026-06-23 19:42:29 +1000

    Wrap statistics in a table within the Statistics component to enhance layout and presentation

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -17,12 +17,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + ' %'} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + ' %'} />
+        </tbody>
+      </table>
     </div>
   )
 }
