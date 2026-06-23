db8dbfc6d3e0a15fac8995816f53956818ac74dd
fatboss19820822 <raymond19820822@gmail.com>
2026-06-23 18:46:27 +1000

    Enhance App component to display a message when no feedback is given, improving user experience and feedback visibility

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -24,7 +24,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  const total = good + neutral + bad
 
 
   return (
@@ -33,7 +34,10 @@ const App = () => {
       <button onClick={() => setGood(good+1)}>good</button>
       <button onClick={() => setNeutral(neutral+1)}>neutral</button>
       <button onClick={() => setBad(bad+1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {total === 0
+        ? <p>No feedback given</p>
+        : <Statistics good={good} neutral={neutral} bad={bad} />
+      }
       
     </div>
   )
