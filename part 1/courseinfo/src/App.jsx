4296b7d4f5837ccf3dea2e2f25d0dbe4b5b504da
fatboss19820822 <raymond19820822@gmail.com>
2026-06-23 16:57:08 +1000

    Implement Unicafe application with feedback functionality, including state management for good, neutral, and bad feedback buttons. Add initial styling and structure for the app, along with necessary assets and configuration files for Vite and ESLint.

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -29,30 +29,54 @@ const Total = ({ parts }) => {
     </p>
 }
 
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+
+//   return (
+//     <div>
+//      <Header course={course.name} />
+//      <Content parts={course.parts} />
+//      <Total parts={course.parts} />
+//     </div>
+//   )
+// }
+ import { useState } from 'react'
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-     <Header course={course.name} />
-     <Content parts={course.parts} />
-     <Total parts={course.parts} />
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
