66636a8ed2c7af96122f14e40685a14d6034717e
fatboss19820822 <raymond19820822@gmail.com>
2026-06-23 18:33:57 +1000

    Refactor App component to extract statistics into a separate Statistics component, improving code organization and readability

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 
+// a proper place to define a component
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const positive =  total === 0 ? 0 : (good / total) * 100
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive =  total === 0 ? 0 : (good / total) * 100
 
 
   return (
@@ -17,13 +33,8 @@ const App = () => {
       <button onClick={() => setGood(good+1)}>good</button>
       <button onClick={() => setNeutral(neutral+1)}>neutral</button>
       <button onClick={() => setBad(bad+1)}>bad</button>
-      <h2>Statisticst</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>total {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      
     </div>
   )
 }
